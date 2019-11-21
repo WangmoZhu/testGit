31c04e77d527d6d232aee9bdd3b2273f0a1ef62e
zwm <zwm@10.211.108.106>
2019-11-21 11:09:47 +0800

    add some pages

---
+++ WebContent/Component.js
@@ -0,0 +1,30 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/Device"
+], function(UIComponent, Device) {
+	"use strict";
+
+	return UIComponent.extend("test.Component", {
+
+		metadata: {
+			manifest: "json"
+		},
+
+		/**
+		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+		 * @public
+		 * @override
+		 */
+		init: function() {
+			// call the base component's init function
+			UIComponent.prototype.init.apply(this, arguments);
+
+			// set the device model
+//			this.setModel(models.createDeviceModel(), "device");
+		},
+		
+//		createContent: function () {
+//            return new sap.m.Label({ text: "Hello!" });
+//        }
+	});
+});
